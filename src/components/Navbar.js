64e260c6dd5b155cd66bdb0814659a6e96bad2b1
onurkaymak <onurkaymak@outlook.com>
2023-11-13 11:38:33 -0800

    WIP layout styling.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,15 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="navbar">NAVBAR</div>
+    <div className="navbar">
+      <div className="navbar-first">
+        <div className="logo">Logo</div>
+        <div className="searchbar">Searchbar</div>
+        <div className="profile">Profile</div>
+      </div>
+      <div className="navbar-second">Second</div>
+
+    </div>
   )
 };
 
